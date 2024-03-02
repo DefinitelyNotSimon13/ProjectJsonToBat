4a8ffca9733a5e10c375b424d6713ec1b818b4d4
DefinitelyNotSimon13 <DefinitelyNotSimon13@users.noreply.github.com>
2024-03-02 14:20:14 +0000

    deploy: f58f17e16031cb47368faac043c8d0d30feca8ec

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['wip_0',['WIP',['../namespaceWIP.html',1,'']]]
+  ['main_0',['main',['../df/d0a/main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
+  ['main_2ecpp_1',['main.cpp',['../df/d0a/main_8cpp.html',1,'']]],
+  ['major_5fversion_2',['MAJOR_VERSION',['../d8/dfd/config_8hpp.html#aa9e8f3bb466bb421d13913df7aeaa20c',1,'config.hpp']]],
+  ['minor_5fversion_3',['MINOR_VERSION',['../d8/dfd/config_8hpp.html#a320988aa2655ee094f3a34a52da10831',1,'config.hpp']]]
 ];
--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['operator_3d_0',['operator=',['../classutils_1_1StartupHandler.html#a2dd3b477a62197fab9761f19b06908d3',1,'utils::StartupHandler']]]
+  ['patch_5fversion_0',['PATCH_VERSION',['../d8/dfd/config_8hpp.html#aed5f6b4b087292b1f6579952fc7c14eb',1,'config.hpp']]],
+  ['project_5fname_1',['PROJECT_NAME',['../d8/dfd/config_8hpp.html#ae73053051efbb45c3a39751f5ce9fb36',1,'config.hpp']]]
 ];
